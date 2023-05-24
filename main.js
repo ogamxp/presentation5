bd36277b3e0d9aecf87e030d2629f158a402bfb5
ogamxp <xyliganistan100mk@gmail.com>
2023-05-24 16:10:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// let Shaykh = {
+//     name: 'Nazrulloh',
+//     surname: 'Abdulloev',
+//     age: 15,
+//     married: true,
+//     study: 'SoftClub',
+//     gender: 'Male',
+//     FullName: function () { 
+//         let fullname = Shaykh.name.concat(' ', this.surname);
+//         return fullname
+//     }
+// }
+
+// console.log(Shaykh.FullName());
+
+// 1
+// let getVoteCount = (obj) => {
+//     return obj.upvotes - obj.downvotes;
+// }
+
+// console.log(getVoteCount({upvotes:13, downvotes:0}));
+// console.log(getVoteCount({upvotes:2, downvotes:33}));
+// console.log(getVoteCount({upvotes:132, downvotes:132}));
+
+// 2
+// let isEmpty = (obj) => {
+//     return Object.keys(obj).length === 0?true:false;
+// }
+
+// console.log(isEmpty({}));
+// console.log(isEmpty({a:1}));
+
+// 3
+let maximumScore = (arr) => {
+    
+}
